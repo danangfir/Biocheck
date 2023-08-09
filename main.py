32c62cc648318afe5016d137512174718e80d061
danangfir <danangfirmino26@gmail.com>
2023-08-09 21:52:02 +0700

    file if

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 print('Ibu berkata, \"Pergi ke toko\"')
 print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab , "Beli satu botol susu, dan jika ada telur beli 6"')
+print("maka budi berangkat ke toko")
+print("dan mulai berbelanja")
+
+# percabangan
+milk_bottle_count = 1
+
+if milk_bottle_count > 0 :
+    print("budi membeli 1 botol susu")
