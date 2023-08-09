ee05bb36ac02a4d8f8faddcee9ef5fb5a2e99dee
danangfir <danangfirmino26@gmail.com>
2023-08-09 08:07:25 +0700

    membuat main code

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-print("ayam")
-print("pitik")
+print('Ibu berkata, \"Pergi ke toko\"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab , "Beli satu botol susu, dan jika ada telur beli 6"')
