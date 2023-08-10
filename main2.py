dd85bfc384d355d34745ad894fa5928759ae6cdd
danangfir <danangfirmino26@gmail.com>
2023-08-10 08:51:14 +0700

    project botol

---
+++ main2.py
@@ -0,0 +1,19 @@
+# contoh project
+
+print("ibu memberi perintah beli 1 botol susu")
+print("anak menjawab ok")
+print("anak pergi ke toko")
+print("apakah susu ada?")
+print("|||||||||||||||")
+bottle_count = 1
+egg_count = 6
+print(f"jumlah botol {bottle_count} botol")
+print(f"jumlah telur {egg_count} telur")
+if bottle_count > 0:
+    print("anak megecek harganya, cukup")
+else:
+    print("anak tidak jadi membeli susu")
+print("anak pulang kerumah")
+print("menyerahkan hasil belanjanya ke ibu")
+print("selesai")
+
