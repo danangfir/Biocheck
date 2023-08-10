1513dee62b521ce1ad24fb0b9064607c67bcc1ea
danangfir <danangfirmino26@gmail.com>
2023-08-10 11:14:23 +0700

    project botol

---
+++ looping.py
@@ -0,0 +1,10 @@
+# Perulangan
+jumlah_buku = 10
+print('ibu berkata, "baca semua bukumu"')
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+jumlah_buku_yang_sudah_dibaca = 0
+for jumlah_buku_yang_sudah_dibaca in range(1, 10):
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f"jumlah buku yang sudah dibaca{jumlah_buku_yang_sudah_dibaca}")
