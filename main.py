0389c5dfdb409b46f34fae3098ed67c5ce44bdd7
danangfir <danangfirmino26@gmail.com>
2023-08-10 07:13:39 +0700

    file if

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab , "Beli satu botol susu, dan jika ada telur beli 6"')
 print("maka budi berangkat ke toko")
 print("dan mulai berbelanja")
+print("||||||||||||||||||||||||")
 
 # percabangan
 milk_bottle_count = 1
+egg_count = 6
 
 if milk_bottle_count > 0 :
+    print("budi mengecek harganya, dan ternyata uangnya cukup")
+
     print("budi membeli 1 botol susu")
+else:
+    print("budi tidak jadi membeli1 botol susu")
+
+print("budi pulang kerumah")
+print("menyampaikan hasilnya kepada ibu")
