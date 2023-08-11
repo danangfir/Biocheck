700ef3fcebbb704c1b639811dc18b4177e4659a1
danangfir <danangfirmino26@gmail.com>
2023-08-11 07:59:00 +0700

    looping

---
+++ looping_undifined.py
@@ -0,0 +1,16 @@
+# Program perulangan membaca buku dengan while
+jumlah_buku = 10
+print("ibu berkata , 'baca semua bukumu'")
+
+jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f"jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
+
+while jumlah_buku_yang_sudah_dibaca_dan_dipahami < jumlah_buku :
+    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9 :
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} belum paham")
+    else:
+        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} Sudah Dibaca dan dipahami ")
+
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
+
--- while.py
+++ while.py
@@ -1,6 +1,6 @@
 # Program perulangan membaca buku dengan while
-jumlah_buku = 100
-print("ibu berkata , 'baca semua bukumu")
+jumlah_buku = 10
+print("ibu berkata , 'baca semua bukumu'")
 
 jumlah_buku_yang_sudah_dibaca = 0
 print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
