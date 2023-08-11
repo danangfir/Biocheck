8f416e7ba3bc910d44b7e130d8095889ed59c51d
danangfir <danangfirmino26@gmail.com>
2023-08-11 08:05:58 +0700

    looping

--- looping_undifined.py
+++ looping_undifined.py
@@ -5,9 +5,10 @@ print("ibu berkata , 'baca semua bukumu'")
 jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
 print(f"jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
 
-while jumlah_buku_yang_sudah_dibaca_dan_dipahami < jumlah_buku :
+while total_jumlah_baca < jumlah_buku * 2 :
+    total_jumlah_baca = total_jumlah_baca + 1
     if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9 :
-        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} belum paham")
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham")
     else:
         jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
         print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} Sudah Dibaca dan dipahami ")
