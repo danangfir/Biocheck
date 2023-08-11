67490158820d8f215974d02ab1a0b6edc9290e91
danangfir <danangfirmino26@gmail.com>
2023-08-11 21:07:31 +0700

    menambahkan fungsi for dan in

---
+++ list.py
@@ -0,0 +1,13 @@
+daftar_buku = ['seven habits', 'how to influence people', 'first things first', '4DX']
+print('tampilkan daftar variable daftar_buku')
+print(daftar_buku)
+
+print('\nproses semua dengan for in')
+for buku in daftar_buku :
+    print(buku)
+print('Tampilkan isi daftar_buku pada indeks tertentu')
+print(daftar_buku[0])
+print(daftar_buku[1])
+
+for i in range (0, len(daftar_buku)) :
+    print(daftar_buku[i])
