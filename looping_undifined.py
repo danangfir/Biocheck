f70283b573b92bb801d9ddd91a5dcb176509c0c4
danangfir <danangfirmino26@gmail.com>
2023-08-11 08:38:34 +0700

    perulangan dengan while dengan jumlah tak terbatas dan ada variabel PEMBERHENTI

--- looping_undifined.py
+++ looping_undifined.py
@@ -1,6 +1,7 @@
 # Program perulangan membaca buku dengan while
 jumlah_buku = 10
 print("ibu berkata , 'baca semua bukumu'")
+total_jumlah_baca = 0
 
 jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
 print(f"jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
@@ -14,4 +15,10 @@ while total_jumlah_baca < jumlah_buku * 2 :
         print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} Sudah Dibaca dan dipahami ")
 
 print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
+if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku :
+    print("Bu, semua buku sudah dibaca dan dipahami")
+else:
+    print(f"Bu, Tidak semua buku bisa dipahami, Budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku")
+
+
 
