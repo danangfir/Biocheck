9e3e3dd41260fb194d5e5b42893455dbb536cc50
danangfir <danangfirmino26@gmail.com>
2023-08-13 11:34:18 +0700

    for in range

--- list.py
+++ list.py
@@ -21,4 +21,41 @@ for buku in daftar_buku :
 
 print('Kembalikan nilai awal daftar buku')
 daftar_buku = ['Seven Habits', 'Bertani Modern', 'Neo-Paleon', 'skibidi']
+print('\n Tambahkan satu buku baru')
 daftar_buku.append('Dunia Matematik kelas 5')
+for i in  range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\n Clear List')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\n Ganti Elemen Pertama')
+daftar_buku = ['Seven Habits', 'Bertani Modern', 'Neo-Paleon', 'skibidi']
+daftar_buku[0] = 'Paleon Modern'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\n ambil elemen ke-2')
+buku = daftar_buku.pop(1)
+print(buku)
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\n Buku yang diambil tadi')
+print(buku)
+
+
+print('\n Pop -1')
+daftar_buku = ['Seven Habits', 'Bertani Modern', 'Neo-Paleon', 'skibidi']
+daftar_buku.pop(-2)
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-angka = 1
+daftar_angka = [1, 2, 3, 4, 5]
 
-while angka <= 5:
-    print(angka)
-    angka += 1
+# Menggunakan pop tanpa argumen
+angka_terhapus = daftar_angka.pop()
+print(angka_terhapus)  # Output: 5
+print(daftar_angka)  # Output: [1, 2, 3, 4]
+
+# Menggunakan pop dengan memberikan indeks
+angka_terhapus = daftar_angka.pop(1)
+print(angka_terhapus)  # Output: 2
+print(daftar_angka)  # Output: [1, 3, 4]
+
+teks = "Halo  dunia!"
+print(len(teks))  # Output: 12
